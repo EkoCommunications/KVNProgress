8ba533f8eafc079119ce583414c207a65ee90fcc
Stan Baranouski <stanislau@ekoapp.com>
2016-08-01 17:13:27 +0700

    Carthage support

---
+++ Source/KVNProgress.h
@@ -0,0 +1,21 @@
+//
+//  KVNProgress.h
+//  KVNProgress
+//
+//  Created by Stan Baranouski on 8/1/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVNProgress.
+FOUNDATION_EXPORT double KVNProgress_VersionNumber;
+
+//! Project version string for KVNProgress.
+FOUNDATION_EXPORT const unsigned char KVNProgress_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVNProgress/PublicHeader.h>
+#import <KVNProgress/KVNProgress.h>
+#import <KVNProgress/KVNProgressConfiguration>
+
+
