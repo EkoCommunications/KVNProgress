b78404d2bbc8fb8c74befcd20805d33d3fdd1bcf
Stan Baranouski <stanislau@ekoapp.com>
2016-08-01 17:13:27 +0700

    Carthage support

---
+++ Source/KVNProgressPublicHeader.h
@@ -0,0 +1,20 @@
+//
+//  KVNProgress.h
+//  KVNProgress
+//
+//  Created by Stan Baranouski on 8/1/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVNProgress.
+FOUNDATION_EXPORT double KVNProgress_VersionNumber;
+
+//! Project version string for KVNProgress.
+FOUNDATION_EXPORT const unsigned char KVNProgress_VersionString[];
+
+#import "KVNProgress.h"
+#import "KVNProgressConfiguration"
+
+
